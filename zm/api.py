470771ff2da31994c9d33d2425569d617571df08
ashish-greycube <mr.ashish.shah@gmail.com>
2019-12-25 10:06:17 +0530

    change to min qty

--- zm/api.py
+++ zm/api.py
@@ -4,7 +4,8 @@ import frappe
 def update_SO_qty_based_on_width_height(self,method):
 	items= self.get("items")
 	for item in items:
-		minimum_width_cf=frappe.db.get_value('Item', item.item_code, 'minimum_width_cf')
-		if item.widht_cf < minimum_width_cf:
-			item.widht_cf=minimum_width_cf
 		item.qty=item.widht_cf*item.height_cf
+		qty_cf=frappe.db.get_value('Item', item.item_code, 'minimum_width_cf')
+		if item.qty < qty_cf:
+			item.qty=qty_cf
+		
