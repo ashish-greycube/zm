f22bb04720ff36492cc78116b74290599f59e6b5
ashish-greycube <mr.ashish.shah@gmail.com>
2020-01-17 17:37:22 +0530

    SI changes

--- zm/api.py
+++ zm/api.py
@@ -1,11 +1,11 @@
 from __future__ import unicode_literals
 import frappe
 
-def update_SO_qty_based_on_width_height(self,method):
-	items= self.get("items")
-	for item in items:
-		item.qty=item.widht_cf*item.height_cf
-		qty_cf=frappe.db.get_value('Item', item.item_code, 'minimum_width_cf')
-		if item.qty < qty_cf:
-			item.qty=qty_cf
+# def update_SO_qty_based_on_width_height(self,method):
+# 	items= self.get("items")
+# 	for item in items:
+# 		item.qty=item.widht_cf*item.height_cf
+# 		qty_cf=frappe.db.get_value('Item', item.item_code, 'minimum_width_cf')
+# 		if item.qty < qty_cf:
+# 			item.qty=qty_cf
 		
